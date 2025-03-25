233587c5fe4ad0c4f5541e9400cf6f5d4ec38fee
udhayakumar <g.udhayakumar17@gmail.com>
2025-03-25 14:52:08 +0530

    Service file

--- code/src/ui/src/services/request-type.service.ts
+++ code/src/ui/src/services/request-type.service.ts
@@ -3,7 +3,11 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-const JSON_FILE_PATH = 'E:/Hackthon 2025/new/gaied-ai-beginners/code/src/ui/assets/request-types.json';
+//const JSON_FILE_PATH = 'E:/Hackthon 2025/new/gaied-ai-beginners/code/src/ui/assets/request-types.json';
+const API_ENDPOINT = 'http://127.0.0.1:8000/request-types/request-types';
+const POST_API_ENDPOINT = 'http://127.0.0.1:8000/request-types/request-types';
+const PUT_API_ENDPOINT = 'http://127.0.0.1:8000/request-types/request-types';
+const Delete_API_ENDPOINT = 'http://127.0.0.1:8000/request-types/request-types';
 const STORAGE_KEY = 'requestTypesData';
 
 @Injectable({
@@ -15,49 +19,64 @@ export class RequestTypeService {
   constructor(private http: HttpClient) {}
 
   // Load data from JSON file or localStorage
+  // loadRequestTypes(): Observable<any> {
+  //   return this.http.get<any>(API_ENDPOINT).pipe(
+  //     map((data) => {
+  //       this.requestTypes = data?.CommercialBankLendingService?.RequestTypes || [];
+  //       return this.requestTypes;
+  //     })
+  //   );
+  // }
   loadRequestTypes(): Observable<any> {
-    const storedData = localStorage.getItem(STORAGE_KEY);
-
-    if (storedData) {
-      this.requestTypes = JSON.parse(storedData);
-      return of(this.requestTypes);
-    } else {
-      return this.http.get<any>(JSON_FILE_PATH).pipe(
-        map((data) => {
-          this.requestTypes = data.CommercialBankLendingService.RequestTypes;
-          localStorage.setItem(STORAGE_KEY, JSON.stringify(this.requestTypes));
-          return this.requestTypes;
-        })
-      );
-    }
+    return this.http.get<any>(API_ENDPOINT);
   }
 
-  // Get all request types
-  getRequestTypes(): Observable<any[]> {
-    return of(this.requestTypes);
+   // Get all request types
+   getRequestTypes(): Observable<any[]> {
+    return this.http.get<any[]>(API_ENDPOINT);
   }
 
   // Add a new request type
+  // addRequestType(requestType: any): Observable<any> {
+  //   this.requestTypes.push(requestType);
+  //   localStorage.setItem(STORAGE_KEY, JSON.stringify(this.requestTypes));
+  //   return of(requestType);
+  // }
   addRequestType(requestType: any): Observable<any> {
-    this.requestTypes.push(requestType);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.requestTypes));
-    return of(requestType);
+    return this.http.post<any>(POST_API_ENDPOINT, requestType);
   }
-
   // Update a request type
-  updateRequestType(updatedRequest: any): Observable<any> {
-    const index = this.requestTypes.findIndex((req) => req.type === updatedRequest.type);
-    if (index !== -1) {
-      this.requestTypes[index] = updatedRequest;
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.requestTypes));
-    }
-    return of(updatedRequest);
-  }
+  // updateRequestType(updatedRequest: any): Observable<any> {
+  //   const index = this.requestTypes.findIndex((req) => req.type === updatedRequest.type);
+  //   if (index !== -1) {
+  //     this.requestTypes[index] = updatedRequest;
+  //     localStorage.setItem(STORAGE_KEY, JSON.stringify(this.requestTypes));
+  //   }
+  //   return of(updatedRequest);
+  // }
+  // Update a request type (using POST instead of PUT)
+  // updateRequestType(updatedRequest: any): Observable<any> {
+  //   return this.http.post<any>(PUT_API_ENDPOINT, updatedRequest); // Using POST as per your request
+  // }
+  // updateRequestType(updatedRequest: any): Observable<any> {
+  //   const url = `${PUT_API_ENDPOINT}/${updatedRequest.type}`; // Ensure the correct resource is updated
+  //   return this.http.put(url, { subRequestTypes: updatedRequest.subRequestTypes }); // Send only updatable fields
+  // }
 
-  // Delete a request type
-  deleteRequestType(type: string): Observable<any> {
-    this.requestTypes = this.requestTypes.filter((req) => req.type !== type);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(this.requestTypes));
-    return of({ message: 'Deleted successfully' });
+  updateRequestType(updatedRequest: any): Observable<any> {
+    const url = `${PUT_API_ENDPOINT}/${encodeURIComponent(updatedRequest.type)}`; // Encode URL for special characters
+    return this.http.put(url, { 
+      type: updatedRequest.type,  // Ensure type is included
+      subRequestTypes: updatedRequest.subRequestTypes 
+    });
+  }
+  // // Delete a request type
+  // deleteRequestType(type: string): Observable<any> {
+  //   this.requestTypes = this.requestTypes.filter((req) => req.type !== type);
+  //   localStorage.setItem(STORAGE_KEY, JSON.stringify(this.requestTypes));
+  //   return of({ message: 'Deleted successfully' });
+  // }
+  deleteRequestType(id: string): Observable<any> {
+    return this.http.delete<any>(`${Delete_API_ENDPOINT}/${id}`);
   }
 }
